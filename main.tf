9261cd4a2c2d19fce4d7288821035050db85754a
louisphilip <72431908+louisphilip@users.noreply.github.com>
2022-03-18 14:47:51 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -128,7 +128,8 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-    Hashicorp = "devops"
+    Department = "devops"
+    Billable = "true"
   }
 }
 
